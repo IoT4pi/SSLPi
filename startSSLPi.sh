5b5f200a8b7b40ed843dcc8bdb5760052110cc31
IoT4pi <office@iot4pi.com>
2016-11-18 12:27:00 +0100

    Add files via upload

---
+++ startSSLPi.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+HOME=/home/pi/Test4/
+Script="SSLserverPi_V1_2.py"
+NumParam=$#
+echo $NumParam
+
+if [ $NumParam = "0" ]; then
+        cd $HOME
+        Test=$HOME$Script
+        echo "NO Parameter"
+else
+        #proof if path ende with /
+        echo $1 | grep '/$' >> /dev/null
+        if [ $? -eq 1 ];then
+                echo "Missing / at the end of Path"
+                Param=$1"/"
+        else
+                Param=$1
+        fi 
+        cd $Param
+        Test=$Param$Script
+        echo "ONE Parameter"
+fi
+
+
+echo "Test = " $Test
+#pwd
+
+
+sudo ps aux | grep python | grep $Test #>> /dev/null
+! [ "$?" = "0" ] && sudo python $Test  >> SSLPi.log
+
+
